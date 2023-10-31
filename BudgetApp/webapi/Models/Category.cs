e85cf84f8b77d0ddb3c587cc288e00fa0eb9ee14
Kaapo Paananen <Kaapo.paana@hotmail.com>
2023-10-31 13:24:44 +0200

    Added a connection to DB, packages EFC.Design, EFC.SqlServer & EFC.Tools & scaffolded

---
+++ BudgetApp/webapi/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Back_End.Models;
+
+public partial class Category
+{
+    public int CategoryId { get; set; }
+
+    public string CategoryName { get; set; } = null!;
+
+    public string? CategoryDescription { get; set; }
+}
