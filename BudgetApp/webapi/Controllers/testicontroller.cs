a4f3929a195d65cf88d65dd38b5e5924a71805e8
jeremiasRosenberg <jeremias.rosenberg@student.careeria.fi>
2023-11-02 15:18:37 +0200

    testi

---
+++ BudgetApp/webapi/Controllers/testicontroller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Back_End.Controllers
+{
+	public class testicontroller : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
