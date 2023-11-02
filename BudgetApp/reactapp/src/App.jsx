2e46dc2a5897da312f815d3d803c28a36ca3fbb2
TalvenlapsiDot <kukkajessika@gmail.com>
2023-11-02 18:29:11 +0200

    Fix ja silti ei toimi prkl.

--- BudgetApp/reactapp/src/App.jsx
+++ BudgetApp/reactapp/src/App.jsx
@@ -1,14 +1,14 @@
 import {Box } from '@chakra-ui/react'
 import React, { Component } from 'react';
-import './global.styles.css'
+import styles from './styles.css'
 
 export default class App extends Component {
     static displayName = App.name;
 
     render() {
         return (
-                <Box w="700px" h="500px" bg="teal.400" p="5" borderRadius="25" boxShadow="xl" marginTop="200" maxWidth="700px" zIndex="1" marginLeft="auto" marginRight="auto">
-                <h1 id="tabelLabel" >FrontPage Here Later Maybe Idk Man</h1>
+                <Box className={styles.Boxes} w="700px" h="600px" bg="whiteAlpha.500" p="5" borderRadius="25" boxShadow="xl" marginTop="150" maxWidth="700px" zIndex="1" marginLeft="auto" marginRight="auto">
+                <h1>FrontPage Here Later Maybe Idk Man</h1>
                 <p>Derp</p>
                 </Box>
         );
--- BudgetApp/reactapp/src/main.jsx
+++ BudgetApp/reactapp/src/main.jsx
@@ -2,7 +2,6 @@ import { ChakraProvider } from '@chakra-ui/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './global.styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ChakraProvider>
