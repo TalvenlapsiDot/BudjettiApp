4d2b1b8f3c3e26c1e53b4ccc561f94bf1e79cc6e
jeremiasRosenberg <jeremias.rosenberg@student.careeria.fi>
2023-11-20 17:38:52 +0200

    controller muutoksia

---
+++ BudgetApp/webapi/Controllers/CategoryController.cs
@@ -0,0 +1,146 @@
+﻿using Back_End.Models;
+using Back_End.Models.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq.Expressions;
+using System.Text.RegularExpressions;
+
+namespace Back_End.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CategoryController : ControllerBase
+	{
+		private BudgetAppDbContext db;
+
+		public CategoryController(BudgetAppDbContext dbparametri)
+		{
+		db = dbparametri;
+		}
+
+		
+		
+		
+		//Add new category
+		[HttpPost]
+		public ActionResult AddNew([FromBody] Category cate)
+		{
+			try
+			{
+				db.Categories.Add(cate);
+				db.SaveChanges();
+				return Ok("Added new category");
+			}
+			catch (Exception ex)
+			{
+				return BadRequest(ex.InnerException);
+			}
+		}
+
+		//Get all categories
+		[HttpGet]
+		public ActionResult GetAllCategories()
+		{
+			try
+			{
+				var cate = db.Categories.ToList();
+				return Ok(cate);
+			}
+			catch (Exception ex)
+			{
+				return BadRequest(ex.InnerException);
+			}
+		}
+
+
+
+
+
+
+
+		//Find Category by id
+		[HttpGet]
+		[Route("{id}")]
+		public ActionResult GetCategoryByName(int id)
+		{
+			try
+			{
+				Category cate = db.Categories.Find(id);
+				if (cate != null)
+				{
+					return Ok(cate);
+				}
+				else
+				{
+					return NotFound("Cannot find category with the id of " + id);
+				}
+			}
+			catch (Exception ex)
+			{
+				return BadRequest(ex.InnerException);
+			}
+		}
+
+		//Find Category with id and Update
+		[HttpPut("{id}")]
+		public ActionResult EditCategory(int id, [FromBody] Category cate)
+		{
+			var cat = db.Categories.Find(id);
+			if (cat != null)
+			{
+				cat.CategoryId = cate.CategoryId;
+				cat.CategoryName = cate.CategoryName;
+				cat.CategoryDescription = cate.CategoryDescription;
+
+				db.SaveChanges();
+				return Ok("Updated Category " + cate.CategoryName);
+			}
+			else
+			{
+				return NotFound("Cannot find Category with the id of " + id);
+			}
+		}
+
+		//Get/Find by catefory name
+		[HttpGet("categoryname/{cname}")]
+		public ActionResult GetByName(string cname)
+		{
+			try
+			{
+				var cate = db.Categories.Where(c => c.CategoryName.Contains(cname));
+				return Ok(cate);
+			}
+			catch (Exception ex)
+			{
+				return BadRequest(ex.Message);
+			}
+		}
+
+
+		//Delete category by id
+		[HttpDelete("{id}")]
+		public ActionResult Delete(int id)
+		{
+			try
+			{
+				var cate = db.Categories.Find(id);
+
+				if (cate != null)
+				{ 
+					db.Categories.Remove(cate);
+					db.SaveChanges();
+					return Ok("Removed " + cate.CategoryName);
+				}
+				else
+				{
+					return NotFound();
+				}
+			}
+			catch (Exception ex)
+			{
+				return BadRequest(ex.InnerException);
+			}
+		}
+
+	}
+}
---
+++ BudgetApp/webapi/Controllers/IncomeController.cs
@@ -0,0 +1,137 @@
+﻿using Back_End.Models;
+using Back_End.Models.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq.Expressions;
+using System.Text.RegularExpressions;
+
+namespace Back_End.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class IncomeController : ControllerBase
+	{
+		private BudgetAppDbContext db;
+
+		public IncomeController(BudgetAppDbContext dbparametri)
+		{
+			db = dbparametri;
+		}
+
+		//Get all incomes
+		[HttpGet]
+		public ActionResult GetAllIncomes()
+		{
+			try
+			{
+				var inc= db.Incomes.ToList();
+				return Ok(inc);
+			}
+			catch (Exception ex)
+			{
+				return BadRequest("Tapahtui virhe. Lue lisää: " + ex.InnerException);
+			}
+		}
+
+		//Add income
+		[HttpPost]
+		public ActionResult AddNew([FromBody]  Income inc)
+		{
+			try
+			{
+				db.Incomes.Add(inc);
+				db.SaveChanges();
+				return Ok("Added new income");
+			}
+			catch (Exception ex)
+			{
+				return BadRequest(ex.InnerException);
+			}
+		}
+
+
+		//Get Incomes by user id
+		[HttpGet]
+		[Route("{id}")]
+		public ActionResult GetIncomeByid(int id)
+		{
+			try
+			{
+				Income inc = db.Incomes.Find(id);
+				if (inc != null)
+				{
+					return Ok(inc);
+				}
+				else
+				{
+					return NotFound("Cannot find income with user id of " + id);
+				}
+			}
+			catch (Exception ex)
+			{
+				return BadRequest(ex.InnerException);
+			}
+		}
+
+		//Get/Find by category name
+		[HttpGet("categoryid/{cid}")]
+		public ActionResult GetByName(int cid)
+		{
+			try
+			{
+				var cate = db.Incomes.Where(c => c.CategoryId == cid);
+				return Ok(cate);
+			}
+			catch (Exception ex)
+			{
+				return BadRequest(ex.Message);
+			}
+		}
+
+		//Find Income with id and Update
+		[HttpPut("{id}")]
+		public ActionResult EditIncome(int id, [FromBody] Income incs)
+		{
+			var inc = db.Incomes.Find(id);
+			if (inc != null)
+			{
+				inc.UserId = incs.UserId;
+				inc.CategoryId = incs.CategoryId;
+				inc.IncomeDate= incs.IncomeDate;
+				inc.IncomeAmount = incs.IncomeAmount;
+
+				db.SaveChanges();
+				return Ok("Updated Income ");
+			}
+			else
+			{
+				return NotFound("Cannot find Income with the user id of " + id);
+			}
+		}
+		//Delete income
+		[HttpDelete("{id}")]
+		public ActionResult Delete(int id)
+		{
+			try
+			{
+				var inc= db.Incomes.Find(id);
+
+				if (inc != null)
+				{
+					db.Incomes.Remove(inc);
+					db.SaveChanges();
+					return Ok("Removed income");
+				}
+				else
+				{
+					return NotFound();
+				}
+			}
+			catch (Exception ex)
+			{
+				return BadRequest(ex.InnerException);
+			}
+		}
+
+	}
+}
