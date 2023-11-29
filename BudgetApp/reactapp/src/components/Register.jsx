5792fff4138b2c418b2bfa6ac442ff37db6a768d
TalvenlapsiDot <kukkajessika@gmail.com>
2023-11-29 17:14:07 +0200

    Register Works

--- BudgetApp/reactapp/src/components/Register.jsx
+++ BudgetApp/reactapp/src/components/Register.jsx
@@ -1,32 +1,36 @@
 import { useState, useCallback } from 'react'
-import { Button, Input, useToast} from '@chakra-ui/react'
+import { Button, Input, Tooltip} from '@chakra-ui/react'
 
-const Register = () => {
-    const toast = useToast();
+const Register = ({onRegister}) => {
+    const [ userName, setUserName] = useState('');
+    const [ passWord, setUserPassword] = useState('');
 
     return (
         <>
+        <Tooltip hasArrow label="Minimum of 5 characters. No special characters other than - or _.">
             <Input
                 width="100%"
                 size='sm'
-                placeholder='Username' /><Input
+                placeholder='Username'
+                value={userName}
+                onChange={(e) => setUserName(e.target.value)}
+                />
+        </Tooltip>
+
+        <Tooltip hasArrow label="Minimum length of 10, make sure it contains atleast one number and one uppercase letter.">
+            <Input
                 width="100%"
                 size='sm'
-                placeholder='Password' /><Button
+                placeholder='Password'
+                value={passWord}
+                onChange={(b) => setUserPassword(b.target.value)}/>
+        </Tooltip>
+            <Button
                 variant='ghost'
                 width="100%"
                 backgroundColor='teal.200'
-                //Turn this into promise-based later
-                // https://chakra-ui.com/docs/components/toast
-                // Make it send info to database
-                onClick={() => toast({
-                    title: 'Account Created.',
-                    description: 'We have created your account successfully',
-                    status: 'success',
-                    duration: 2000,
-                    isClosable: true,
-                })}
-            >
+                onClick={() => onRegister(userName, passWord)}
+                >
                 Register
             </Button>
         </>
