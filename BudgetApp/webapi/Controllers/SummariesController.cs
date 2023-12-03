b05c47dfb9e367a902f25a3c8d3e634f5010c226
KSPaananen <133219434+KSPaananen@users.noreply.github.com>
2023-12-03 11:58:56 +0200

    Api/Summary ready (#10)
    * SummarizeSetDates ready
    
    * SummarizeThreeMonths ready
    
    * SummarizeCurrentMonth ready
    
    * Added budget to summaries
    
    * Code cleanup. Ready for pr

--- BudgetApp/webapi/Controllers/SummariesController.cs
+++ BudgetApp/webapi/Controllers/SummariesController.cs
@@ -3,7 +3,6 @@ using Back_End.Models.Context;
 using Back_End.Models.Views;
 using Back_End.Services;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -21,46 +20,230 @@ namespace Back_End.Controllers
             _context = context;
             _configuration = configuration;
         }
-
-        [HttpGet,Authorize]
-        [Route("SummarizeSetDates")]
+        
+        [HttpGet, Authorize]
+        [Route("SummarizeSetDates/{StartingDate};{EndingDate}")]
         public IActionResult SummarizeSetDates(DateTime StartingDate, DateTime EndingDate)
         {
-            // Read users name from token nameclaim and get users data from db
+            // Read username from token nameclaim and get users data
             string ClaimName = JsonWebTokenService.GetUniqueName(Request, _configuration);
             List<User> UserList = _context.Users.FromSql($"SELECT * FROM USERS WHERE Username = {ClaimName}").ToList();
+
+            if (StartingDate.GetHashCode() == 0 || EndingDate.GetHashCode() == 0)
+            {
+                return StatusCode(StatusCodes.Status204NoContent, "Invalid or empty dates!");
+            }
 
             // Query reqired data
             List<Income> IncomeList = _context.Incomes.FromSql($"SELECT * FROM Income WHERE IncomeDate >= {@StartingDate} AND IncomeDate <= {@EndingDate} AND UserID = {UserList[0].UserId}").ToList();
             List<Expenditure> ExpenditureList = _context.Expenditures.FromSql($"SELECT * FROM Expenditure WHERE ExpenditureDate >= {@StartingDate} AND ExpenditureDate <= {@EndingDate} AND UserID = {UserList[0].UserId}").ToList();
+            List<Budget> BudgetList = _context.Budgets.FromSql($"SELECT * FROM Budget WHERE StartDate >= {StartingDate} AND EndDate <= {EndingDate} AND UserID = {UserList[0].UserId}").ToList();
 
-            double TotalIncome = 0;
-            double TotalExpenditure = 0;
+            // Calculate total income and expenditure
+            double TotalIncome = 0, TotalExpenditure = 0, TotalBudget = 0;
 
             for (int i = 0; i < IncomeList.Count(); i++)
             {
-                TotalIncome = TotalIncome + IncomeList[i].IncomeAmount;
+                TotalIncome += IncomeList[i].IncomeAmount;
             }
 
             for (int i = 0; i < ExpenditureList.Count(); i++)
             {
-                TotalExpenditure = TotalExpenditure + ExpenditureList[i].ExpenditureAmount;
+                TotalExpenditure += ExpenditureList[i].ExpenditureAmount;
             }
 
+            for (int i = 0; i < BudgetList.Count(); i++)
+            {
+                TotalBudget += BudgetList[i].BudgetAmount;
+            }
+
+            // Figure out the most common category for income and expenditure
+            var IncomeCategoryID = IncomeList.GroupBy(i => i.CategoryId)
+                .OrderByDescending(group => group.Count())
+                .Select(group => group.Key)
+                .FirstOrDefault();
+
+            var IncomeCategory = from c in _context.Categories
+                                 where c.CategoryId == IncomeCategoryID
+                                 select c.CategoryName;
+
+            var ExpenditureCategoryID = ExpenditureList.GroupBy(i => i.CategoryId)
+                .OrderByDescending(group => group.Count())
+                .Select(group => group.Key)
+                .FirstOrDefault();
+
+            var ExpenditureCategory = from c in _context.Categories
+                                      where c.CategoryId == ExpenditureCategoryID
+                                      select c.CategoryName;
+
+            // Create summary
             MonthlySummary monthlySummary = new MonthlySummary
             {
                 StartingDate = StartingDate,
                 EndingDate = EndingDate,
                 TotalIncome = TotalIncome,
                 TotalExpenditure = TotalExpenditure,
-                NetValue = TotalIncome - TotalExpenditure,
-                MostCommonIncomeCategory = "",
-                MostCommonExpenditureCategory = "",
+                NetAmount = TotalIncome - TotalExpenditure,
+                BudgetRemaining = TotalBudget - TotalExpenditure,
+                MostCommonIncomeCategory = IncomeCategory.FirstOrDefault(),
+                MostCommonExpenditureCategory = ExpenditureCategory.FirstOrDefault(),
             };
 
-            return StatusCode(StatusCodes.Status418ImATeapot, monthlySummary);
+            return StatusCode(StatusCodes.Status200OK, monthlySummary);
         }
 
-        
+        // This needs optimization
+        [HttpGet, Authorize]
+        [Route("SummarizeThreeMonths")]
+        public IActionResult SummarizeThreeMonths()
+        {
+            // Read username from token nameclaim and get users data
+            string ClaimName = JsonWebTokenService.GetUniqueName(Request, _configuration);
+            List<User> UserList = _context.Users.FromSql($"SELECT * FROM USERS WHERE Username = {ClaimName}").ToList();
+
+            // Add summaries to this list
+            List<MonthlySummary> SummariesList = new List<MonthlySummary>();
+
+            // Build a summary in a loop
+            for (int i = 0; i < 3; i++)
+            {
+                DateTime StartingDate = DateTime.Now.Date.AddMonths(-3 + i);
+                StartingDate = StartingDate.AddDays(-DateTime.Today.Day + 1);
+                DateTime EndingDate = DateTime.Now.Date.AddMonths(-2 + i);
+                EndingDate = EndingDate.AddDays(-DateTime.Today.Day);
+
+                // Query reqired data
+                List<Income> IncomeList = _context.Incomes.FromSql($"SELECT * FROM Income WHERE IncomeDate >= {StartingDate} AND IncomeDate <= {EndingDate} AND UserID = {UserList[0].UserId}").ToList();
+                List<Expenditure> ExpenditureList = _context.Expenditures.FromSql($"SELECT * FROM Expenditure WHERE ExpenditureDate >= {StartingDate} AND ExpenditureDate <= {EndingDate} AND UserID = {UserList[0].UserId}").ToList();
+                List<Budget> BudgetList = _context.Budgets.FromSql($"SELECT * FROM Budget WHERE StartDate >= {StartingDate} AND EndDate <= {EndingDate} AND UserID = {UserList[0].UserId}").ToList();
+
+                double TotalIncome = 0, TotalExpenditure = 0, TotalBudget = 0;
+
+                // Calculate total income for the month
+                foreach (var income in IncomeList)
+                {
+                    TotalIncome += income.IncomeAmount;
+                }
+
+                // Calculate total expenditure for the month
+                foreach (var expenditure in ExpenditureList)
+                {
+                    TotalExpenditure += expenditure.ExpenditureAmount;
+                }
+
+                foreach (var budget in BudgetList)
+                {
+                    TotalBudget += + budget.BudgetAmount;
+                }
+
+                // Figure out the most common category for income and expenditure
+                var IncomeCategoryID = IncomeList.GroupBy(i => i.CategoryId)
+                    .OrderByDescending(group => group.Count())
+                    .Select(group => group.Key)
+                    .FirstOrDefault();
+
+                var IncomeCategory = from c in _context.Categories
+                                     where c.CategoryId == IncomeCategoryID
+                                     select c.CategoryName;
+
+                var ExpenditureCategoryID = ExpenditureList.GroupBy(i => i.CategoryId)
+                    .OrderByDescending(group => group.Count())
+                    .Select(group => group.Key)
+                    .FirstOrDefault();
+
+                var ExpenditureCategory = from c in _context.Categories
+                                          where c.CategoryId == ExpenditureCategoryID
+                                          select c.CategoryName;
+
+                // Create summary and add it to the list
+                MonthlySummary Summary = new MonthlySummary
+                {
+                    StartingDate = StartingDate,
+                    EndingDate = EndingDate,
+                    TotalIncome = TotalIncome,
+                    TotalExpenditure = TotalExpenditure,
+                    NetAmount = TotalIncome - TotalExpenditure,
+                    BudgetRemaining = TotalBudget - TotalIncome,
+                    MostCommonIncomeCategory = IncomeCategory.FirstOrDefault(),
+                    MostCommonExpenditureCategory = ExpenditureCategory.FirstOrDefault(),
+                };
+
+                SummariesList.Add(Summary);
+            }
+
+            return StatusCode(StatusCodes.Status200OK, SummariesList);
+        }
+
+        [HttpGet, Authorize]
+        [Route("SummarizeCurrentMonth")]
+        public IActionResult SummarizeCurrentMonth()
+        {
+            // Read username from token nameclaim and get users data
+            string ClaimName = JsonWebTokenService.GetUniqueName(Request, _configuration);
+            List<User> UserList = _context.Users.FromSql($"SELECT * FROM USERS WHERE Username = {ClaimName}").ToList();
+
+            // Set dates to first day of the month to current day
+            DateTime BeginningOfTheMonth = DateTime.Now.AddDays(-DateTime.Today.Day + 1);
+            DateTime Today = DateTime.Now;
+
+            // Query reqired data
+            List<Income> IncomeList = _context.Incomes.FromSql($"SELECT * FROM Income WHERE IncomeDate >= {BeginningOfTheMonth} AND IncomeDate <= {Today} AND UserID = {UserList[0].UserId}").ToList();
+            List<Expenditure> ExpenditureList = _context.Expenditures.FromSql($"SELECT * FROM Expenditure WHERE ExpenditureDate >= {BeginningOfTheMonth} AND ExpenditureDate <= {Today} AND UserID = {UserList[0].UserId}").ToList();
+            List<Budget> BudgetList = _context.Budgets.FromSql($"SELECT * FROM Budget WHERE StartDate >= {BeginningOfTheMonth} AND EndDate <= {Today} AND UserID = {UserList[0].UserId}").ToList();
+
+            // Calculate total income for the month
+            double TotalIncome = 0, TotalExpenditure = 0, TotalBudget = 0;
+
+            foreach (var income in IncomeList)
+            {
+                TotalIncome += income.IncomeAmount;
+            }
+
+            // Calculate total expenditure for the month
+            foreach (var expenditure in ExpenditureList)
+            {
+                TotalExpenditure += expenditure.ExpenditureAmount;
+            }
+
+            foreach (var budget in BudgetList)
+            {
+                TotalBudget += budget.BudgetAmount;
+            }
+
+            // Figure out the most common category for income and expenditure
+            var IncomeCategoryID = IncomeList.GroupBy(i => i.CategoryId)
+                .OrderByDescending(group => group.Count())
+                .Select(group => group.Key)
+                .FirstOrDefault();
+
+            var IncomeCategory = from c in _context.Categories
+                                 where c.CategoryId == IncomeCategoryID
+                                 select c.CategoryName;
+
+            var ExpenditureCategoryID = ExpenditureList.GroupBy(i => i.CategoryId)
+                .OrderByDescending(group => group.Count())
+                .Select(group => group.Key)
+                .FirstOrDefault();
+
+            var ExpenditureCategory = from c in _context.Categories
+                                      where c.CategoryId == ExpenditureCategoryID
+                                      select c.CategoryName;
+
+            // Create summary and add it to the list
+            MonthlySummary Summary = new MonthlySummary
+            {
+                StartingDate = BeginningOfTheMonth,
+                EndingDate = Today,
+                TotalIncome = TotalIncome,
+                TotalExpenditure = TotalExpenditure,
+                NetAmount = TotalIncome - TotalExpenditure,
+                BudgetRemaining = TotalBudget - TotalIncome,
+                MostCommonIncomeCategory = IncomeCategory.FirstOrDefault(),
+                MostCommonExpenditureCategory = ExpenditureCategory.FirstOrDefault(),
+            };
+
+            return StatusCode(StatusCodes.Status200OK, Summary);
+        }
+
     }
 }
--- BudgetApp/webapi/Models/Views/MonthlySummary.cs
+++ BudgetApp/webapi/Models/Views/MonthlySummary.cs
@@ -3,11 +3,13 @@
     public class MonthlySummary
     {
         public DateTime StartingDate { get; set; }
-        public DateTime EndingDate { get; set;}
+        public DateTime EndingDate { get; set; }
         public double TotalIncome { get; set; }
         public double TotalExpenditure { get; set; }
-        public double NetValue { get; set; }
+        public double NetAmount { get; set; }
+        public double BudgetRemaining { get; set; }
         public string? MostCommonIncomeCategory { get; set; }
         public string? MostCommonExpenditureCategory { get; set; }
+
     }
 }
